39d5d70c744aa399947b2f387b9f3b10aade0da1
Julian Armstrong <julian.armstrong8@gmail.com>
2024-08-12 14:52:43 -0600

    Completed exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SchoolSubect();
+        }
+
+        public static void SchoolSubect()
+        {
+            Console.WriteLine("What is your favorite subject in school?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToUpper())
+            {
+                case "ENGLISH":
+                    Console.WriteLine("I used to love English!");
+                    break;
+                case "MATH":
+                    Console.WriteLine("Math was always hard for me.");
+                    break;
+                case "HISTORY":
+                    Console.WriteLine("History was very interesting!");
+                    break;
+                case "SCIENCE":
+                    Console.WriteLine("Science was always fun!");
+                    break;
+                case "HEALTH":
+                    Console.WriteLine("Health was my favorite too!");
+                    break;
+                default:
+                    Console.WriteLine("Good choice!");
+                    break;
+            }
         }
     }
 }
